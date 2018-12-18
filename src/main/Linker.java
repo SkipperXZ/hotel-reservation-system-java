f93c9a84ad525a9f411534d1385a72e5c8604596
heart <heartmannet@hotmail.com>
2018-12-19 00:48:36 +0700

    calendarUI

--- src/main/Linker.java
+++ src/main/Linker.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 
 public class Linker {
     public static Stage primaryStage;
-    public static Scene DashboardScene ;
     public static Scene resScene ;
     public static Scene customerScene;
     public static Scene user;
